1829d1600094a85ffd9c55fc816e0c4267a4ba90
Rakibul Hasan <shurovmohammad@gmail.com>
2023-07-06 02:29:18 -0700

    2nd commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7z$ueemb2r=m_9uk_0-b%bmq-vh&7m@(w3e)0o5*(081e)r6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
